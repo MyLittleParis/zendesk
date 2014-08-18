182f2f628297ec0c678b2170e3283a3fe012b595
Mathieu Ferment <mathieu.ferment@mylittleparis.com>
2014-08-18 11:05:28 +0200

    Enable ticket comments update

--- src/Dlin/Zendesk/Client/BaseClient.php
+++ src/Dlin/Zendesk/Client/BaseClient.php
@@ -1,9 +1,9 @@
 <?php
 /**
  *
- * User: davidlin
- * Date: 5/11/2013
- * Time: 10:03 AM
+ * User: matks
+ * Date: 18/8/2014
+ * Time: 11:04 AM
  *
  */
 
@@ -26,7 +26,6 @@ abstract class BaseClient
      */
     protected $api;
 
-
     /**
      *
      * Constructor
@@ -48,13 +47,10 @@ abstract class BaseClient
         $entity->setManagingClient($this);
     }
 
-
-
     /**
      * @return string
      */
     abstract public function getType();
-
 
     /**
      * Generic function for getting a collection of entities
@@ -79,7 +75,6 @@ abstract class BaseClient
             $query->set('sort_by', $sort_by)->set('sort_order', $sort_order == 'asc' ? 'asc' : 'desc');
         }
 
-
         $response = $this->processRequest($request);
 
 
@@ -91,8 +86,6 @@ abstract class BaseClient
         if (array_key_exists('count', $values)) {
             $result->setCount($values['count']);
         }
-
-
 
         /*
         if (array_key_exists('next_page', $values)) {
@@ -117,8 +110,6 @@ abstract class BaseClient
             }
         }
         return $result;
-
-
     }
 
 
@@ -130,7 +121,6 @@ abstract class BaseClient
      */
     protected function getOne($end_point)
     {
-
         $end_point = strtolower($end_point);
         if (strpos($end_point, 'http') !== 0) {
             $end_point = $this->api->getApiUrl() . $end_point;
@@ -156,36 +146,36 @@ abstract class BaseClient
             $t->setManagingClient($this);
             return $t->fromArray($result[$alternativeName]);      
         }
-        return null;
 
+        return null;
     }
 
     /**
-     * Create an entity
+     * Create or update an entity
      *
      * @param BaseEntity $entity
      * @param $endPoint
+     * @param $extraData fields to update
      * @return ChangeResult|null
      */
     public function saveEntity(BaseEntity $entity, $endPoint='', $extraData=null)
     {
         $end_point = strtolower($endPoint);
-
         if (strpos($end_point, 'http') !== 0) {
             $end_point = $this->api->getApiUrl() . $end_point;
         }
+
         $type = $this->getType();
         $className = explode('\\', $type);
         $baseName = strtolower(end($className));
 
         $method = $entity->getId() ? 'put':'post';
-        if($method == 'post'){
+        if ($method == 'post') {
             $entity->checkCreatable();
         }
 
-
         $changes = $entity->toArray(true, $extraData);
-        if(empty($changes)){
+        if (empty($changes)) {
             return null;
         }
 
@@ -193,18 +183,20 @@ abstract class BaseClient
         $response = $this->processRequest($request);
         $result = $response->json();
 
-
         if ($result && isset($result[$baseName])) {
             $changeResult = new ChangeResult();
             $t = new $type();
             $this->manage($t);
+
             $t->fromArray($result[$baseName]);
             $changeResult->setItem($t);
+
             if (isset($result['audit'])) {
                 $audit = new TicketAudit();
                 $audit->fromArray($result['audit']);
                 $changeResult->setAudit($audit);
             }
+
             return $changeResult;
         }
         return null;
@@ -219,28 +211,21 @@ abstract class BaseClient
             $end_point = $this->api->getApiUrl() . $end_point;
         }
         $response = $this->api->delete($end_point)->send();
-        return $response->getStatusCode() == 200;
 
+        return $response->getStatusCode() == 200;
     }
 
-
-
-
     public function deleteByIds(array $ids, $end_point=''){
-
-
-
         if (strpos($end_point, 'http') !== 0) {
             $end_point = $this->api->getApiUrl() . $end_point;
         }
+
         $request = $this->api->delete($end_point);
         $request->getQuery()->set('ids',implode(',',$ids) );
         $response=$request->send();
+
         return $response->getStatusCode() == 200;
-
     }
-
-
 
     /**
      * Process request into a response object
@@ -251,10 +236,7 @@ abstract class BaseClient
      */
     public function processRequest(RequestInterface $request)
     {
-
-
         $response = $request->send();
-
 
         $attempt = 0;
         while ($response->getStatusCode() == 429 && $attempt < 5) {
@@ -269,7 +251,6 @@ abstract class BaseClient
         if ($response->getStatusCode() >= 500) {
             throw new ZendeskException('Zendesk Server Error Detected.');
         }
-
 
         if ($response->getStatusCode() >= 400) {
             if ($response->getContentType() == 'application/json') {
